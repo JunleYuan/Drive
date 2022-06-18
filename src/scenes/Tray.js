632282be8ca59ef34657dbab4858d6a040598acc
JunleYuan <leo905991717@gmail.com>
2022-06-17 23:12:30 -0700

    key scene

--- src/scenes/Tray.js
+++ src/scenes/Tray.js
@@ -14,9 +14,11 @@ class Tray extends Phaser.Scene {
 
         this.background = this.add.image(0, -100, 'tray').setOrigin(0, 0).setScale(1);
 
+        
+
         //this.background = this.add.image(0, -100, 'trayKey').setOrigin(0, 0).setScale(1);
 
-        this.key = this.add.image(0, 0, 'key').setOrigin(0, 0).setScale(1.5).setInteractive();
+        this.key = this.add.image(0, 0, 'key').setOrigin(0, 0).setScale(1.5).setInteractive().setDepth(2);;
 
         this.input.setDraggable(this.key);
         
@@ -26,13 +28,49 @@ class Tray extends Phaser.Scene {
             gameObject.y = dragY;
     
         });
-        
+
+
+        var zone = this.add.zone(580, 200, 700, 300).setRectangleDropZone(700, 300);
+
+        //  Just a visual display of the drop zone
+        var graphics = this.add.graphics();
+        graphics.lineStyle(2, 0xffff00);
+        graphics.strokeRect(zone.x - zone.input.hitArea.width / 2, zone.y - zone.input.hitArea.height / 2, zone.input.hitArea.width, zone.input.hitArea.height);
+
+        this.input.on('dragenter', function (pointer, gameObject, dropZone) {
+
+            graphics.clear();
+            graphics.lineStyle(2, 0x00ffff);
+            graphics.strokeRect(zone.x - zone.input.hitArea.width / 2, zone.y - zone.input.hitArea.height / 2, zone.input.hitArea.width, zone.input.hitArea.height);
+    
+        });
+    
+        this.input.on('dragleave', function (pointer, gameObject, dropZone) {
+    
+            graphics.clear();
+            graphics.lineStyle(2, 0xffff00);
+            graphics.strokeRect(zone.x - zone.input.hitArea.width / 2, zone.y - zone.input.hitArea.height / 2, zone.input.hitArea.width, zone.input.hitArea.height);
+    
+        });
+    
+        this.input.on('drop', this.setBack(this,this.key));
     }
 
     update() {
 
 
 
+
+
+    }
+
+    setBack(scene,key){
+
+        return function() {
+            scene.background.setTexture('trayKey');
+
+            scene.key.visible = false;
+        };
 
 
     }
